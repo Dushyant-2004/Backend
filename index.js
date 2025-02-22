16a014eed86fb7a64163a921bac95e3a41bb3959
Dushyant-2004 <dushyantvasisht@gmail.com>
2025-02-22 12:31:05 +0530

    Add new folders and files

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-// const express = require('express');
-// const app = express();
-
-// const port = 8000;
-
-// app.set('view engine', 'ejs');
-
-// app.get('/', (req, res) => {
-//     res.render("home.ejs");
-
-//     });
-
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-//     });
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 3000;
+const path = require('path');
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get("/register", (req, res) => {
-    res.send("Register get page");
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+let posts = [
+    {
+        username: 'John',
+        content: 'Hello World',
+    },
+    {
+        username: 'Jane',
+        content: 'Hi, there!',
+    },
+    {
+        username: 'Doe',
+        content: 'Hello, everyone!',
+    },
+];
+
+
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { posts });
 });
 
-app.post("/register", (req, res) => {
-    console.log(req.body);
-    res.send("Register post page");
-}); 
-
-
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
